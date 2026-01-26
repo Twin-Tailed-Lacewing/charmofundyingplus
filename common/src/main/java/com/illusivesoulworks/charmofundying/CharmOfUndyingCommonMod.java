470cdefed6b58501f84eb9cdc4e596e54bf9f53f
Twin-Tailed-Lacewing <ian.a.dwyer1@gmail.com>
2026-01-26 18:27:08 -0500

    Give HOLLOW_TOTEM to player on effect application
    Add functionality to give a HOLLOW_TOTEM to the player when effects are applied. Needs registry modifcation.

--- common/src/main/java/com/illusivesoulworks/charmofundying/CharmOfUndyingCommonMod.java
+++ common/src/main/java/com/illusivesoulworks/charmofundying/CharmOfUndyingCommonMod.java
@@ -67,6 +67,11 @@ public class CharmOfUndyingCommonMod {
 
       if (effectProvider.applyEffects(livingEntity, damageSource, copy)) {
         Services.PLATFORM.broadcastTotemEvent(livingEntity);
+
+  			ItemStack _setstack = new ItemStack(TotemReuseModItems.HOLLOW_TOTEM.get()).copy();
+	  		_setstack.setCount(1);
+	  		ItemHandlerHelper.giveItemToPlayer(livingEntity, _setstack);
+        
         return true;
       }
     }
